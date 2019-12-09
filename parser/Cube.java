55b0984234eed37e38064507ab2f4f5507648079
Justin James RONALD <jronald@c3r1s3.wethinkcode.co.za>
2019-12-09 20:29:29 +0200

    working on instructions

--- parser/Cube.java
+++ parser/Cube.java
@@ -1,19 +1,20 @@
 package parser;
 
+import java.util.*;
 import parser.Plane;
 
 public class Cube{
 
-    private Plane Front;
-    private Plane Back;
-    private Plane Up;
-    private Plane Down;
-    private Plane Left;
-    private Plane Right;
-    private int dimensions;
+    private  Plane Front;
+    private  Plane Back;
+    private  Plane Up;
+    private  Plane Down;
+    private  Plane Left;
+    private  Plane Right;
+    private  int dimensions;
+    private HashMap <String, String[]> sides;
 
-
-    public Cube (int dimension){
+    public Cube(final int dimension) {
         dimensions = dimension;
         Front = new Plane('F', dimensions);
         Back = new Plane('B', dimensions);
@@ -21,16 +22,78 @@ public class Cube{
         Down = new Plane('D', dimensions);
         Left = new Plane('L', dimensions);
         Right = new Plane('R', dimensions);
+        sides = new HashMap<String, String[]>();
+        sides.put("U", new String[]{"F", "L", "B", "R"});
+        sides.put("D", new String[]{"F", "L", "B", "R"});
+        sides.put("L", new String[]{"U", "F", "D", "B"});
+        sides.put("R", new String[]{"U", "F", "D", "B"});
+        sides.put("B", new String[]{"U", "L", "D", "R"});
+        sides.put("F", new String[]{"U", "L", "D", "R"});
     }
     
-    public int readInstructions(String instructions){
-        for (String temp : instructions.split(" ")){
-            switch(temp){
-                
-                case "U":
-                    break;
-            }
+
+    public int readInstructions(final String instructions) {
+        char[] tem = new char[dimensions];
+        for (String temp : instructions.split(" ")) {
+            
         }
         return (0);
+    }
+
+    public void rotation(String face, int direction, int rot){
+        char [][] temp = new char [][] {getFaceRotation(sides.get(face)[0], rot), 
+            getFaceRotation(sides.get(face)[1], rot), getFaceRotation(sides.get(face)[2], rot), 
+            getFaceRotation(sides.get(face)[3], rot)};
+        if (direction == 1){
+            for (int i = 0; i < sides.get(face).length; i++){
+                faceParser(face, rot, temp[i]);
+            }
+        }else if (direction == -1){
+            for (int i = sides.get(face).length - 1; i >= 0 ; i--){
+                faceParser(face, rot, temp[i]);
+            }
+        }
+    }
+
+    public void faceParser(String face, int rotation, char [] row){
+        switch(face){
+            case "F":
+                Front.setRotate(rotation, row);
+                break;
+            case "B":
+                Back.setRotate(rotation, row);
+                break;
+            case "L":
+                Left.setRotate(rotation, row);
+                break;
+            case "R":
+                Right.setRotate(rotation, row);
+                break;
+            case "U":
+                Up.setRotate(rotation, row);
+                break;
+            case "D":
+                Down.setRotate(rotation, row);
+                break;
+        }
+    }
+
+    public char[] getFaceRotation(String face, int rotation){
+        switch(face){
+            case "F":
+                return Front.getRotate(rotation);
+            case "B":
+                return Back.getRotate(rotation);
+            case "L":
+                return Left.getRotate(rotation);
+            case "R":
+                return Right.getRotate(rotation);
+            case "U":
+                return Up.getRotate(rotation);
+            case "D":
+                return Down.getRotate(rotation);
+            default:
+                return null;
+        }
     }
 }
--- parser/Plane.java
+++ parser/Plane.java
@@ -1,56 +1,94 @@
 package parser;
 
-public class Plane{
+public class Plane {
 
     private char[][] face;
     private int dimensions;
     private char rep;
-    
-    public Plane(char c, int dim){
+
+    public Plane(char c, int dim) {
         rep = c;
         dimensions = dim;
-        for (int i = 0; i < dim ; i ++){
-            for (int j = 0; j < dim ; j++){
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
                 face[i][j] = c;
             }
         }
     }
 
-    public int setRow(int ind, char[] row){
-        if (ind >= dimensions || ind < dimensions){
-            return (-1); 
+    public int setRotate(int ind, char[] row) {
+        if (ind < 1 || ind > 4) {
+            return (-1);
+        }
+        switch (ind) {
+        case 1:
+            setRow(1, row);
+            break;
+        case 2:
+            setRow(3, row);
+            break;
+        case 3:
+            setColumn(1, row);
+            break;
+        case 4:
+            setColumn(3, row);
+            break;
+        }
+        return (0);
+    }
+
+    public char[] getRotate(int ind) {
+        if (ind < 1 || ind > 4) {
+            return null;
+        }
+        switch (ind) {
+            case 1:
+                return getRow(1);
+            case 2:
+                return getRow(3);
+            case 3:
+                return getColumn(1);
+            case 4:
+                return getColumn(3);
+            default:
+                return null;
+        }
+    }
+
+    public int setRow(int ind, char[] row) {
+        if (ind >= dimensions || ind < dimensions) {
+            return (-1);
         }
         face[ind] = row;
         return (0);
     }
 
-    public int setColumn(int ind, char[] col){
-        if (ind >= dimensions || ind < dimensions){
-            return (-1); 
+    public int setColumn(int ind, char[] col) {
+        if (ind >= dimensions || ind < dimensions) {
+            return (-1);
         }
-        for (int i = 0; i < dimensions; i++){
+        for (int i = 0; i < dimensions; i++) {
             face[i][ind] = rep;
         }
         return (0);
     }
 
-    public char[] getRow(int ind){
-        if (ind >= dimensions || ind < dimensions){
-            return null; 
+    public char[] getRow(int ind) {
+        if (ind >= dimensions || ind < dimensions) {
+            return null;
         }
         return face[ind];
     }
 
-    public char[] getColumn(int ind){
-        if (ind >= dimensions || ind < dimensions){
-            return null; 
+    public char[] getColumn(int ind) {
+        if (ind >= dimensions || ind < dimensions) {
+            return null;
         }
-        char [] res = new char[9];
-        for (int i = 0; i < dimensions ; i++){
+        char[] res = new char[9];
+        for (int i = 0; i < dimensions; i++) {
             res[i] = face[ind][i];
         }
         return res;
     }
-
 
 }
